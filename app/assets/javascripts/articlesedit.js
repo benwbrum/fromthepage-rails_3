8812109d61d9a9b6d6c9a19dd82cb68bced34501
Eric MacAdie <Eric@MacAdie.net>
2013-05-22 07:30:15 -0500

    I at least got some JS to work, no JQuery yet

--- app/assets/javascripts/articlesedit.js
+++ app/assets/javascripts/articlesedit.js
@@ -1,4 +1,6 @@
-/*  */
+// That took forever
+// alert("hello")
+/*  
 // Append the function to the "document ready" chain
 jQuery(function($) {
   // when the #search field changes
@@ -10,29 +12,33 @@ jQuery(function($) {
     });
 })
 
-sert a notice between the last comment and the comment form */
+sert a notice between the last comment and the comment form *
 $("#comment-notice").html('<div class="flash notice"><%= escape_javascript(flash.delete(:notice)) %></div>');
  
-/* Replace the count of comments */
+/* Replace the count of comments *
 $("#comments_count").html("<%= pluralize(Comment.count, 'Comment') %>");
  
-/* Add the new comment to the bottom of the comments list */
+/* Add the new comment to the bottom of the comments list *
 $("#comments").append("<%= escape_javascript(render(@comment)) %>");
  
-/* Highlight the new comment */
+/* Highlight the new comment *
 $("#comment_<%= @comment.id %>").effect("highlight", {}, 3000);
  
-/* Reset the comment form */
+/* Reset the comment form *
 $("#new_comment")[0].reset();
 
+jQuery(function($) {
 $(document).ready(function(){
     $("p").click(function(){
 	$(this).hide();
     });
 });
+})
 
 $(document).ready(function(){
     $("li").click(function(){
 	$(this).hide();
     });
 });
+
+*/
